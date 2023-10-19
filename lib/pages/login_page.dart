88c48bdc8b05128ad2ae27c99705aafe7b08cd0d
atifkhan77 <atifm1190@gmail.com>
2023-10-19 21:35:47 +0500

    adding dark and light mode

---
+++ lib/pages/login_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.background,
+      body: Center(
+        child: Column(),
+      ),
+    );
+  }
+}
